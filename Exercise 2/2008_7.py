769d0d9af052d98551319249e80ec9def1358dc4
jitladapr <63130047@kmitl.ac.th>
2021-08-20 20:12:15 +0700

    exercise 2

---
+++ Exercise 2/2008_7.py
@@ -0,0 +1,20 @@
+"""
+Simple Calculator + - * / %
+"""
+
+FirstInput = float(input("Input first number: "))
+Operator = input("Input your operator: ")
+SecondInput = float(input("Input second number: "))
+
+if(Operator == '+'):
+    print(round((FirstInput + SecondInput), 2))
+elif(Operator == '-'):
+    print(round((FirstInput - SecondInput), 2))
+elif(Operator == '*'):
+    print(round((FirstInput * SecondInput), 2))
+elif(Operator == '/'):
+    print(round((FirstInput / SecondInput), 2))
+elif(Operator == '%'):
+    print(round((FirstInput % SecondInput), 2))
+else:
+    print("ERROR")
